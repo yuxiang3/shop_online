f8bdda1b541a3fe63d1b0c68807d9db341bfe4b5
yuxiang <565671781@qq.com>
2023-11-09 08:02:36 +0800

    搭建项目基础框架

---
+++ src/main/java/com/example/demo/constant/Constant.java
@@ -0,0 +1,14 @@
+package com.example.demo.constant;
+
+
+/**
+ * @author ycshang
+ */
+public interface Constant {
+    //    创建时间
+    String CREATE_TIME = "createTime";
+    //    更新时间
+    String UPDATE_TIME = "updateTime";
+    //    逻辑删除
+    String DELETE_FLAG = "deleteFlag";
+}
---
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author ycshang
+ */
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    UNAUTHORIZED(401, "登录失效，请重新登录"),
+    INTERNAL_SERVER_ERROR(500, "服务器异常，请稍后再试");
+
+    private final int code;
+    private final String msg;
+}
